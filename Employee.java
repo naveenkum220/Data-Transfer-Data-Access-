d8066aa3dac75f9527eba447dcddffa9991392ce
Naveen <130820720+naveenkum220@users.noreply.github.com>
2024-12-16 18:52:22 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,1 +1,40 @@
+package com.dto;
 
+public class Employee {
+	private int id;
+	private String name;
+	private Long mobno;
+	private double sal;
+	public Employee(int id, String name, Long mobno, double sal) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobno = mobno;
+		this.sal = sal;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getMobno() {
+		return mobno;
+	}
+	public void setMobno(Long mobno) {
+		this.mobno = mobno;
+	}
+	public Double getSal() {
+		return sal;
+	}
+	public void setSal(Double sal) {
+		this.sal = sal;
+	}
+	
+}
